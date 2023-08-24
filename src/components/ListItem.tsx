8a0ab7dc2fcf4a7b7f90830d4b8a0a7195d50616
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2023-08-24 17:17:06 +0300

    add theme colors

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -20,7 +20,7 @@ function ListItem({children, selected, className} : IListItem) {
         <li className={classes}>
             <div className="flex items-center pl-3">
                 <input id="todo-checkbox" type="checkbox" value="" checked={selected}
-                       className="transition w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                       className="transition w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
                     <label htmlFor="todo-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                         {children}
                     </label>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // tailwind theme
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -17,6 +18,23 @@ module.exports = {
   // override theme
   theme: {
     // alternative mx-auto
+    colors: {
+      // default theme colors
+      primary: {
+        '50': '#fff1f1',
+        '100': '#ffe1e1',
+        '200': '#ffc7c7',
+        '300': '#ffa0a0',
+        '400': '#ff6b6b',
+        '500': '#f83b3b',
+        '600': '#e51d1d',
+        '700': '#c11414',
+        '800': '#a01414',
+        '900': '#841818',
+        '950': '#480707',
+      },
+      ...colors
+    },
     container: {
       center: true,
     },
