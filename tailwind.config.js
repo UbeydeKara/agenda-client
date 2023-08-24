b6aa58d140aeb1bd2963f9930d03feb285319e07
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2023-08-24 15:20:07 +0300

    add safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 // tailwind theme
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   // toggling dark mode manually, use the class strategy instead of the media strategy
   darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  // tailwind doesn't support dynamic classes, need to use safelist
+  safelist: [
+    {
+      pattern: /space-(x|y)-(2|4)/,
+    },
   ],
   // override theme
   theme: {
