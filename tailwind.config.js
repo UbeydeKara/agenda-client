3cf4a39183b12a076f95f5b70c82c9cd8c4a5eb7
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2023-08-24 14:02:57 +0300

    toggling dark mode manually

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  // toggling dark mode manually, use the class strategy instead of the media strategy
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
